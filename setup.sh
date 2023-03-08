bce28cc2872641a1b9baf826da30b1bd425cb7b2
aj-goldie <101531405+aj-goldie@users.noreply.github.com>
2023-03-07 22:15:09 -0600

    setup file to download zsh and .zshrc

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update the package index and install zsh
+sudo apt-get update && sudo apt-get install -y zsh
+
+# Download the .zshrc file from GitHub
+wget https://raw.githubusercontent.com/username/repo/main/.zshrc -O ~/.zshrc
+
+# Change the default shell to zsh
+chsh -s $(which zsh)
