94eeddd7682cf8c35290af62991ec8eea73b3682
aj-goldie <101531405+aj-goldie@users.noreply.github.com>
2023-03-07 22:48:48 -0600

    updated to mkdir before installing alias and function

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 
 # Update the package index and install zsh
 sudo apt-get update && sudo apt-get install -y zsh
-
-# Download the .zshrc file from GitHub
+# Make the ~/.zsh directory for your alias and function files.
+mkdir -p ~/.zsh
+# Install the .zshrc file into the ~/.zshrc folder, aliases.zsh file into the ~/.zsh/aliases.zsh folder, 
+# and the functions.zsh file into the ~/.zsh/functions.zsh folder
 wget https://raw.githubusercontent.com/aj-goldie/remotezsh/main/.zshrc -O ~/.zshrc
 wget https://raw.githubusercontent.com/aj-goldie/remotezsh/main/aliases.zsh -O ~/.zsh/aliases.zsh
 wget https://raw.githubusercontent.com/aj-goldie/remotezsh/main/functions.zsh -O ~/.zsh/functions.zsh
